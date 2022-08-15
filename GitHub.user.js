a78648e11a362934b0bce5a3377814e5e10d2e69
Akash Kakkar <akash.galaxy07@gmail.com>
2022-08-15 17:43:30 +0530

    Kickstart the development

---
+++ GitHub.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        GitHub Accessibility Enhancements
+// @namespace   butterknife/github
+// @description Enhances and improves the accessibility of GitHub
+// @match       https://github.com/*
+// @grant       none
+// @version     1.0
+// @author      Akash Kakkar
+// @copyright 2022 Akash Kakkar
+// @license                     GNU AFFERO GENERAL PUBLIC LICENSE version 3.0
+// ==/UserScript==
