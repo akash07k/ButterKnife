f9f24347529b498c60f7af06353f1f91ff72c943
Akash Kakkar <akash.galaxy07@gmail.com>
2022-08-25 01:52:25 +0530

    fix(GitHub): updated the selectors

--- src/scripts/content/GitHub/tweaks.js
+++ src/scripts/content/GitHub/tweaks.js
@@ -14,7 +14,7 @@ export const LOAD_TWEAKS = [];
 export const DYNAMIC_TWEAKS = [
     {
         // Prenotification control (The notification control which displays before logging in)
-        selector: `#repository-container-header > div.d-flex.flex-wrap.flex-justify-end.mb-3.px-3.px-md-4.px-lg-5 > ul > li:nth-child(2) > a`,
+        selector: `#repository-container-header > div.d-flex > ul.pagehead-actions > li:nth-child(2) > a.tooltipped-s`,
         tweak: (element) => {
             // Convert it into a button for screen reader users
             // We don't want to change the label so don't pass any argument for that
@@ -24,7 +24,7 @@ export const DYNAMIC_TWEAKS = [
 
     // Prefork control (The fork control which displays before logging in)
     {
-        selector: `#repository-container-header > div.d-flex.flex-wrap.flex-justify-end.mb-3.px-3.px-md-4.px-lg-5 > ul > li:nth-child(3) > a`,
+        selector: `#repository-container-header > div.d-flex > ul.pagehead-actions > li:nth-child(3) > a.tooltipped-s`,
         tweak: (element) => {
             // Convert it into a button for screen reader users
             // We don't want to change the label so don't pass any argument for that
@@ -35,7 +35,7 @@ export const DYNAMIC_TWEAKS = [
 
     // Fork repository control.
     {
-        selector: `#repository-container-header > div.d-flex.flex-wrap.flex-justify-end.mb-3.px-3.px-md-4.px-lg-5 > ul > li:nth-child(3) > div > a`,
+        selector: `#repository-container-header > div.d-flex > ul > li:nth-child(2) > div > a.BtnGroup-item`,
         tweak: (element) => {
             // Convert it into a button for screen reader users
             // We don't want to change the label so don't pass any argument for that
@@ -46,7 +46,7 @@ export const DYNAMIC_TWEAKS = [
 
     // Prestar control
     {
-        selector: `#repository-container-header > div.d-flex.flex-wrap.flex-justify-end.mb-3.px-3.px-md-4.px-lg-5 > ul > li:nth-child(4) > div > a`,
+        selector: `#repository-container-header > div.d-flex > ul > li:nth-child(4) > div > a.tooltipped-s`,
         tweak: (element) => {
             // Convert it into a button for screen reader users
             // We don't want to change the label so don't pass any argument for that
@@ -57,7 +57,7 @@ export const DYNAMIC_TWEAKS = [
     },
     // Star button
     {
-        selector: `#repository-container-header > div.d-flex > ul.pagehead-actions > li > div.js-toggler-container > form > button.js-toggler-target`,
+        selector: `#repository-container-header > div.d-flex > ul > li:nth-child(3) > div > form.starred.js-social-form.BtnGroup-parent.flex-auto.js-deferred-toggler-target > button`,
         tweak: (element) => {
             // Define the hotkey: `Alt+Shift+8`
             _common.setHotkey(element, `Alt+Shift+*`, `Alt+Shift+8`);
