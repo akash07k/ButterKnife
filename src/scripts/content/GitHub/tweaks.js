42843afb96d1e4d8b45fd7ddabb0a7143272e3f4
Akash Kakkar <akash.galaxy07@gmail.com>
2022-08-25 01:27:04 +0530

    feat(GitHub): added hotkey for `star repository` button
    The hotkey is: `Alt+Shift+8`

--- src/scripts/content/GitHub/tweaks.js
+++ src/scripts/content/GitHub/tweaks.js
@@ -51,8 +51,19 @@ export const DYNAMIC_TWEAKS = [
             // Convert it into a button for screen reader users
             // We don't want to change the label so don't pass any argument for that
             _common.makeButton(element);
+            // Define the hotkey: `Alt+Shift+8`
+            _common.setHotkey(element, `Alt+Shift+*`, `Alt+Shift+8`);
         },
     },
+    // Star button
+    {
+        selector: `#repository-container-header > div.d-flex > ul.pagehead-actions > li > div.js-toggler-container > form > button.js-toggler-target`,
+        tweak: (element) => {
+            // Define the hotkey: `Alt+Shift+8`
+            _common.setHotkey(element, `Alt+Shift+*`, `Alt+Shift+8`);
+        },
+    },
+
     // Issue listing
     {
         selector: `div.js-issue-row`,
@@ -68,6 +79,7 @@ export const DYNAMIC_TWEAKS = [
         // Issues tab
         selector: `#issues-tab`,
         tweak: (element) => {
+            // Define hotkey: `Alt+Shift+I`
             _common.setHotkey(element, `Alt+I`, `Alt+Shift+i`);
         },
     },
