7d19e79e85098f6ad21561e818e29d293547a631
Akash Kakkar <akash.galaxy07@gmail.com>
2022-11-01 22:14:58 +0530

    feat(universal): added hotkey for quickly copying the HTML element selector to the clipboard
    * the key is `Alt+Shift+1`

---
+++ src/scripts/content/universal/index.js
@@ -0,0 +1,36 @@
+import * as $ from "jquery";
+import Browser from "webextension-polyfill";
+import { install } from "@github/hotkey";
+import hotkeys from "hotkeys-js";
+import * as _tweaks from "./tweaks";
+import * as _common from "../../../libs/common";
+
+const LOAD_TWEAKS = _tweaks.LOAD_TWEAKS;
+const DYNAMIC_TWEAKS = _tweaks.DYNAMIC_TWEAKS;
+const DYNAMIC_TWEAK_ATTRIBUTES = _tweaks.DYNAMIC_TWEAK_ATTRIBUTES;
+
+function installHotkeys() {
+    for (const element of document.querySelectorAll(`[data-hotkey]`)) {
+        install(element);
+    }
+}
+
+$(document).ready(() => {
+    _common.init(LOAD_TWEAKS, DYNAMIC_TWEAKS, DYNAMIC_TWEAK_ATTRIBUTES);
+    installHotkeys();
+    hotkeys(
+        `alt+shift+1,alt+shift+2,alt+shift+3,alt+shift+4,alt+shift+5`,
+        (event, handler) => {
+            switch (handler.key) {
+                case `alt+shift+1`:
+                    selector = _common.getElementSelector(event);
+                    _common.copyElementSelector(event);
+                    event.preventDefault();
+                    break;
+
+                default:
+                    break;
+            }
+        },
+    );
+});
