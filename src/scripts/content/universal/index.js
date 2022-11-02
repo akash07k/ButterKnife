6328bfb0e3576a0cac30f8afd4f9c1812b465c3a
Akash Kakkar <akash.galaxy07@gmail.com>
2022-11-03 01:04:41 +0530

    feat(universal): added TorchMode
    ToarchMode will be helpful for fetching the selectors and source for all elements even in screen reader specific modes

--- src/scripts/content/universal/index.js
+++ src/scripts/content/universal/index.js
@@ -18,10 +18,44 @@ function installHotkeys() {
 $(document).ready(() => {
     _common.init(LOAD_TWEAKS, DYNAMIC_TWEAKS, DYNAMIC_TWEAK_ATTRIBUTES);
     installHotkeys();
+
+    // TorchMode is the mode where we can get the element selectors as well as HTML code of all the elements including noninteractive ones.
+    // It will effectively work even in screen reader specific modes.
+    // It can be toggled by pressing `Alt+Shift+A`.
+    // Clicking functionality won't work while user has turned on this mode and clicking will copy the CSS selector of the element to the clipboard.
+    // Pressing `Alt+Shift+3` will copy the element HTML source to the clipboard.
+    // This mode will be disabled by default.
+    let isTorchModeEnabled = false;
+    // This is the root event which will return the info about the currently clicked element
+    let torchLevelEvent;
+
+    function hookTorchMode() {
+        $(document).click(() => {
+            if (isTorchModeEnabled) {
+                _common.copyElementSelector(event);
+                torchLevelEvent = event;
+                event.preventDefault();
+            }
+        });
+    }
+
+    // Let's hook the event for enable TorchMode to work
+    hookTorchMode();
     hotkeys(
-        `alt+shift+1,alt+shift+2,alt+shift+3,alt+shift+4,alt+shift+5`,
+        `alt+shift+a,alt+shift+1,alt+shift+2,alt+shift+3,alt+shift+4,alt+shift+5`,
         (event, handler) => {
             switch (handler.key) {
+                case `alt+shift+a`:
+                    if (!isTorchModeEnabled) {
+                        isTorchModeEnabled = true;
+                    } else if (isTorchModeEnabled) {
+                        isTorchModeEnabled = false;
+                    }
+
+                    event.preventDefault();
+
+                    break;
+
                 case `alt+shift+1`:
                     _common.copyElementSelector(event);
                     event.preventDefault();
@@ -31,6 +65,11 @@ $(document).ready(() => {
                     event.preventDefault();
                     break;
                 }
+                case `alt+shift+3`:
+                    if (isTorchModeEnabled) {
+                        _common.copyElementHTML(torchLevelEvent);
+                    }
+                    break;
                 default:
                     break;
             }
